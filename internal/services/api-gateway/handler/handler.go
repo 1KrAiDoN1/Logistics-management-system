5e1deb285c8290195f88fcbf1af6ebac11a75daf
1KrAiDoN1 <pavelvasilev24843@gmail.com>
2025-08-19 23:57:26 +0300

    handlers

--- internal/services/api-gateway/handler/handler.go
+++ internal/services/api-gateway/handler/handler.go
@@ -1,40 +1,16 @@
 package handler
 
 import (
-	"logistics/configs"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"log/slog"
+	authpb "logistics/api/protobuf/auth_service"
 )
 
-type Handler struct {
-	authGRPCClient      authpb.UserServiceClient
-	orderGRPCClient     orderpb.OrderServiceClient
-	driverGRPCClient    driverpb.DriverServiceClient
-	warehouseGRPCClient warehousepb.WarehouseServiceClient
-	routeGRPCClient     routepb.RouteServiceClient
+type Handlers struct {
+	AuthHandlerInterface
 }
 
-func NewHandler(
-	authClient authpb.UserServiceClient,
-	orderClient orderpb.OrderServiceClient,
-	driverClient driverpb.DriverServiceClient,
-	warehouseClient warehousepb.WarehouseServiceClient,
-	routeClient routepb.RouteServiceClient,
-) *Handler {
-	return &Handler{
-		authGRPCClient:      authClient,
-		orderGRPCClient:     orderClient,
-		driverGRPCClient:    driverClient,
-		warehouseGRPCClient: warehouseClient,
-		routeGRPCClient:     routeClient,
-	}
-}
-
-func (h *Handler) SignUp(c *gin.Context) {
-	var req configs.MicroservicesConfig // Из shared/models/dto
-	if err := c.BindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+func NewHandlers(logger *slog.Logger, authGRPCClient authpb.AuthServiceClient) *Handlers {
+	return &Handlers{
+		AuthHandlerInterface: NewAuthHandler(logger, authGRPCClient),
 	}
 }
